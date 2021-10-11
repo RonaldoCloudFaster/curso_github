771330b72dc156a081081ae1e1cb3c972b3ae292
Ronaldo Filho <ronaldonogueirafilho@gmail.com>
2021-10-11 16:20:01 -0300

    Adicionando funcionalidade de mostrar a mensagem

--- cadastro.js
+++ cadastro.js
@@ -2,3 +2,9 @@ function signUp(email){
     return email;
 }
 // pegando o email e cadastro na plataforma.
+
+function mostraMensagem(){
+    return 'Bem vindo ao sistema';
+}
+
+//adicionando funcao de mensagem
